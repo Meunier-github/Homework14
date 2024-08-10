9e92d780510cb8b297c68f81a2d2ad5396a606e4
meunier-github <meunier.natalia@gmail.com>
2024-08-10 19:39:22 +0200

    Homework14

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,9 +1,9 @@
-public class Bicycle {
+public class Bicycle extends Vehicle {
 
-    public String modelName;
-    public int wheelsCount;
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
+
+
 }
